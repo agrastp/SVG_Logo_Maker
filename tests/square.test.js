22a82e82f180a32fc7836dab4ae5dd61a506c5b3
AgrastP <agrastp@yahoo.com>
2023-11-02 14:51:54 -0400

    testing

--- tests/square.test.js
+++
@@ -1,9 +0,0 @@
-const Square = require('../lib/square');
-
-describe('square', () => {
-    it('should render the correct shape and color', () => {
-        const square = new Square();
-        var color = "red"
-        expect(square.generateShape().toEqual(`<rect width="300" height="300" fill= "${color}"`));
-    });
-});
